cffb8c1fcd352fbf2ecd89639ca46e3661b124f3
Frederic-25 <frederic4007@gmail.com>
2026-07-12 21:11:20 +0530

    commit 2

---
+++ java_learing/src/string_methods/string_problems.java
@@ -0,0 +1,27 @@
+package string_methods;
+import java.util.*;
+public class string_problems {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a name:");
+		String name = sc.nextLine();
+		/*
+		System.out.print("the length of the name:"+ name.length());
+		*/
+		/*
+		String upper = name.toUpperCase();
+		System.out.print("the uppercase of the name:"+ upper );
+		*/
+		/*
+		if (name.startsWith("A")) {
+			System.out.println("the name start with A");
+		}else {
+			System.out.println("the name dosent start with A");
+		}
+		*/
+	}
+
+}
